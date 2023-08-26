5c2c04a87d87b2aedccd0fd22c89cb4a9c467288
XiaoPanPanKevinPan <XiaoPanPanKevinPan@users.noreply.github.com>
2023-08-27 03:19:21 +0800

    index.mjs - better shorthand()
    - now takes an optional argument called obj and assign the results
    	onto it.

--- index.mjs
+++ index.mjs
@@ -2,14 +2,17 @@ import * as scanl from "./scanl.mjs";
 import { fs, ps, path, os } from "./common.mjs";
 import * as print from "./print.mjs";
 
-const shorthand = async () => {
-	const { isClosed, ...stdioShorhand } = (await scanl.createLineScanner()).shorthand();
-	return {
+const shorthand = async (obj) => {
+	const { isEof: lsIsEof, isEmpty: lsIsEmpty, close: lsClose, ...stdioShorhand } = (await scanl.createLineScanner()).shorthand();
+	const result =  {
 		fs, ps, path, os,
 		...scanl, // EOF, EOL, sscan, sscanf, createLineScanner
-		...stdioShorhand, // getLine, scan, scanf, lineScanner
+		lsIsEof, lsIsEmpty, lsClose, ...stdioShorhand, // getLine, scan, scanf, lineScanner
 		...print // print, printl
 	}
+	if(obj instanceof Object)
+		Object.assign(obj, result);
+	return result;
 }
 
 export { scanl, print, shorthand, fs, ps, path, os };
